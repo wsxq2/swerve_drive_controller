176034fcdb30ca52e7a9d28ffada7086ae468ce5
wsxq2 <wsxq222222@gmail.com>
2026-02-10 17:46:03 +0800

    try to use 120 degree

--- src/swerve_drive_kinematics.cpp
+++ src/swerve_drive_kinematics.cpp
@@ -82,8 +82,8 @@ std::array<WheelCommand, 4> SwerveDriveKinematics::optimize_wheel_commands(
 
     double angle_diff = angles::shortest_angular_distance(current_angle, target_angle);
 
-    // ensure target angle is within +/- 90 degrees
-    if (std::abs(target_angle) > M_PI_2)
+    // ensure target angle is within +/- 120 degrees (actual use 119)
+    if (std::abs(target_angle) > 2.07694)
     {
       optimized_commands[i].drive_angular_velocity = -wheel_commands[i].drive_angular_velocity;
 
